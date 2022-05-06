1cb3c6c2ee10bd02f7f0b260c277c861c7598d25
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-07 01:38:15 +0600

    done

---
+++ __server_deploy.js
@@ -0,0 +1,12 @@
+/*
+ONE TIME
+1.create heroku account
+2.verify email
+3.install heroku cli
+4.heroku login
+--------------------
+for each time project
+1.heroku create
+2.make sure you: git add . git commit. git push
+3.git push heroku main
+*/
