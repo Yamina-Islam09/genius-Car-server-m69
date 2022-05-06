b4b1f529543d174b22f8679156dc0574e6844998
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-07 01:53:36 +0600

    added hero updates

--- __server_deploy.js
+++ __server_deploy.js
@@ -6,7 +6,21 @@ ONE TIME
 4.heroku login
 --------------------
 for each time project
+----------------------
 1.heroku create
 2.make sure you: git add . git commit. git push
 3.git push heroku main
+4.go to heroku app>dashboard>current project>settings>reveal config vars
+5.copy paste config vars from your .env file
+6.make sure u have whitelisted all ip adress to access mongodb
+------------------------------------
+UPDATE SERVER with new changes
+-----------------------------
+1.make changes
+2.make sure you: git add . git commit. git push
+3.git push heroku main
+---------------------------------
+Connect server with client
+----------------------
+
 */
